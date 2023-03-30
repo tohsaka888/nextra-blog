b757ad3247dacf597c4221ec0942978686baefac
tohsaka888 <156132264@qq.com>
2023-03-30 18:44:16 +0800

    ✨ feat(config): theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,6 +10,19 @@ const config: DocsThemeConfig = {
     link: "https://discord.com",
   },
   docsRepositoryBase: "https://github.com/tohsaka888/nextra-blog",
+  footer: {
+    text: '',
+    component: <></>
+  },
+  toc: {
+    title: "本页条目",
+  },
+  feedback: {
+    content: "发现有错误？给我反馈问题☞"
+  },
+  editLink: {
+    text: "编辑本页内容☞"
+  }
 };
 
 export default config;
